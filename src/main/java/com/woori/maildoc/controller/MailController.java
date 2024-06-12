79bb20437b319a0150ea7c2915f13ff0f7cdcc58
DJ._.Dazzling <hers010801@gmail.com>
2024-06-12 14:18:00 +0900

    add

--- src/main/java/com/woori/maildoc/controller/MailController.java
+++ src/main/java/com/woori/maildoc/controller/MailController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,12 +22,11 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/mail")
-@CrossOrigin(origins = "http://localhost:80", allowedHeaders = "*")
 public class MailController {
 	
 	private final MailService mailService;
 	
-	@PostMapping("")
+	@PostMapping()
 	public ResponseEntity<List<ResultRes>> fix(@RequestBody MailReq mailReq) {
 		System.out.println("ddddddddd");
 		log.info("contents  :  " + mailReq.getContents());
