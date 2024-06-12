e5ed03cea31c761c1324a31a967a6d11c5b9fe1e
DJ._.Dazzling <hers010801@gmail.com>
2024-06-12 11:25:47 +0900

    add

---
+++ src/main/java/com/woori/maildoc/dto/resquest/MailReq.java
@@ -0,0 +1,13 @@
+package com.woori.maildoc.dto.resquest;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class MailReq {
+	String contents;
+}
